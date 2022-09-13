bef778724e4445324be6caae6c35df747f965a59
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-09-13 18:31:08 -0300

    fix: adjust migration to make workflow_id unique on scenarios table

--- db/migrations/20220912170943_create_scenarios_table.js
+++ db/migrations/20220912170943_create_scenarios_table.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable("scenarios", (table) => {
     table.uuid("id").primary();
-    table.uuid("workflow_id").notNullable();
+    table.uuid("workflow_id").notNullable().unique();
     table.string("total_scenarios", 255).notNullable();
     table.jsonb("scenarios").notNullable();
     table.timestamps(true, true);
