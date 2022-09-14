bac246d2684ea9202e3ec09c8b554f51b49cb9f5
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-09-14 19:42:08 -0300

    refactor: add method to get external api token

---
+++ src/utils/api.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
+
+const getToken = async () => {
+  const existingToken = process.env.TOKEN;
+  
+  if (!existingToken || !jwt.verify(existingToken, '1234', (err) => err ? false : true)) {
+    process.env.TOKEN = await axios.post(`${process.env.FLOWBUILD_URL}/token`)
+      .then((response) => response.data.jwtToken)
+      .catch((error) => {
+        logger.error(error.message);
+        return;
+      });
+    return process.env.TOKEN;
+  } else {
+    return existingToken;
+  }
+}
+
+const flowbuildApi = axios.create({
+  baseURL: process.env.FLOWBUILD_URL
+});
+
+module.exports = {
+  flowbuildApi,
+  getToken
+}
